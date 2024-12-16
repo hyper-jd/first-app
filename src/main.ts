fa7e0d3c583ce50c228339609d9bc186cf0cf5df
Jenry De La Cruz <jenrydelacruz@Mac.lan>
2024-12-15 20:12:01 -0800

    Initial commit: Upload first-app project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
